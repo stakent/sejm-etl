0d636178fe367603dfb0ea216d1eb8102fcf3653
Dariusz Walat <>
2025-06-24 21:14:59 +0200

    feat: run code using multithreading

--- data_sources/sejm/api.py
+++ data_sources/sejm/api.py
@@ -1,7 +1,9 @@
+import concurrent.futures
 from enum import StrEnum
 from datetime import datetime
 import logging
 import os
+import time
 import requests
 from settings.settings import get_settings
 
@@ -65,7 +67,7 @@ def get_and_cache_act_pdf(act_info: ActInfo) -> None:
         format=ActFormatType.PDF,
     )
     os.makedirs(os.path.dirname(pdf_file_path), exist_ok=True)
-    logging.info(f"Saving {pdf_file_path}")
+    logging.debug(f"Saving {pdf_file_path}")
     with open(pdf_file_path, "wb") as pdf_file:
         pdf_file.write(pdf_data)
 
@@ -76,10 +78,12 @@ def tranform_pdf_to_txt(act_info: ActInfo) -> None:
         base_dir=settings.cache_base_dir,
         format=ActFormatType.PDF,
     )
-    logging.info(f"Transforming PDF to TXT {pdf_file_path}")
+    logging.debug(f"Transforming PDF to TXT {pdf_file_path}")
     try:
         with open(pdf_file_path, "rb") as pdf_file:
-            text = act_pdf_to_text(pdf_file=pdf_file, act_position=act_info.pos)
+            text = act_pdf_to_text(
+                pdf_file=pdf_file, act_position=act_info.pos, log_prefix=pdf_file_path
+            )
     except PageHeaderNotFound as exc:
         logging.error(
             f"Extracting text from pdf {pdf_file_path} - Page header not found error: {exc}"
@@ -96,7 +100,7 @@ def tranform_pdf_to_txt(act_info: ActInfo) -> None:
         pdf_file.write(text)
 
 
-def update_sejm_act_data(act_info: ActInfo) -> None:
+def update_sejm_act_data(act_info: ActInfo, log_prefix: str) -> None:
     if not is_act_cached(act_info=act_info, format=ActFormatType.PDF):
         get_and_cache_act_pdf(act_info=act_info)
 
@@ -104,28 +108,63 @@ def update_sejm_act_data(act_info: ActInfo) -> None:
         tranform_pdf_to_txt(act_info=act_info)
 
 
+tasks_done = 0
+tasks_cancelled = 0
+tasks_aborted_by_exeception = 0
+
+
+def future_callback(future):
+    global tasks_done, tasks_cancelled, tasks_aborted_by_exeception
+    if future.done():
+        tasks_done += 1
+        if future.cancelled():
+            tasks_cancelled += 1
+        else:
+            try:
+                future.result()
+            except Exception as e:
+                tasks_aborted_by_exeception += 1
+    else:
+        tasks_cancelled += 1
+
+
 def update_sejm_acts_data():
     """Run the ETL pipeline for acts."""
     logging.info("Started running the ETL pipeline for acts")
-    settings.number_of_years_to_process = 3
     publishers = ["DU", "MP"]
 
     current_year = datetime.now().year
-    for year in range(
-        current_year, current_year - settings.number_of_years_to_process, -1
-    ):
-        for publisher in publishers:
-            logging.info(f"Processing year: {year} for publishers: {publisher}")
-            acts_list = get_acts_list(publisher, year)
+    futures = []
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=os.cpu_count() - 1
+    ) as executor:
+        for year in range(
+            current_year, current_year - settings.number_of_years_to_process, -1
+        ):
+            for publisher in publishers:
+                logging.info(f"Processing year: {year} for publishers: {publisher}")
+                acts_list = get_acts_list(publisher, year)
+                n = 0
+                number_of_acts = len(acts_list)
+                for act_info in acts_list:
+                    n += 1
+                    if n % 100 == 0:
+                        logging.info(f"Queued {n} acts out of {len(acts_list)}")
+                    log_prefix = f"act {n}/{number_of_acts}"
+                    futures.append(
+                        executor.submit(update_sejm_act_data, act_info, log_prefix)
+                    )
+        for future in futures:
+            future.add_done_callback(future_callback)
+
+        while len(futures) > (
+            tasks_done + tasks_cancelled + tasks_aborted_by_exeception
+        ):
             logging.info(
-                f"Processing publisher: {publisher}, year: {year}, number of acts: {len(acts_list)}"
+                f"Processing {len(futures)} acts, done: {tasks_done}, cancelled: {tasks_cancelled}, aborted: {tasks_aborted_by_exeception} "
             )
-            n = 0
-            for act_info in acts_list:
-                n += 1
-                if n % 100 == 0:
-                    logging.info(f"Processed {n} acts out of {len(acts_list)}")
+            time.sleep(1)
 
-                update_sejm_act_data(act_info=act_info)
-
+    # wait for all futures to complete before exiting the program
+    concurrent.futures.wait(futures)
     logging.info("Finished ETL pipeline for acts")
